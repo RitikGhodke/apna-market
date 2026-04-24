851b25b34f49e4e586d76f0d1a69e68aecaf6207
ritik ghodke <ritikghodke70@gmail.com>
2026-04-24 14:58:09 +0530

    fix: add redux persist safe for nextjs

--- src/context/ReduxProvider.js
+++ src/context/ReduxProvider.js
@@ -1,12 +1,35 @@
+// 'use client';
+
+// import { Provider } from 'react-redux';
+// import { store } from '@/redux/store';
+
+// export default function ReduxProvider({ children }) {
+//   return (
+//     <Provider store={store}>
+//       {children}
+//     </Provider>
+//   );
+// }
+
+
+
+
+
+
+
+
 'use client';
 
 import { Provider } from 'react-redux';
-import { store } from '@/redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from '@/redux/store';
 
 export default function ReduxProvider({ children }) {
   return (
     <Provider store={store}>
-      {children}
+      <PersistGate loading={null} persistor={persistor}>
+        {children}
+      </PersistGate>
     </Provider>
   );
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,20 +1,81 @@
+// import { configureStore } from '@reduxjs/toolkit';
+// import authReducer from './slices/authSlice';
+// import cartReducer from './slices/cartSlice';
+// import shopReducer from './slices/shopSlice';
+// import orderReducer from './slices/orderSlice';
+
+// export const store = configureStore({
+//   reducer: {
+//     auth: authReducer,
+//     cart: cartReducer,
+//     shop: shopReducer,
+//     order: orderReducer,
+//   },
+//   middleware: (getDefaultMiddleware) =>
+//     getDefaultMiddleware({
+//       serializableCheck: false,
+//     }),
+// });
+
+// export default store;
+
+
+
+
+
+
+
 import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import { combineReducers } from 'redux';
 import authReducer from './slices/authSlice';
 import cartReducer from './slices/cartSlice';
 import shopReducer from './slices/shopSlice';
 import orderReducer from './slices/orderSlice';
 
+// Next.js SSR ke liye safe storage
+const createNoopStorage = () => ({
+  getItem() { return Promise.resolve(null); },
+  setItem() { return Promise.resolve(); },
+  removeItem() { return Promise.resolve(); },
+});
+
+const storage = typeof window !== 'undefined'
+  ? require('redux-persist/lib/storage').default
+  : createNoopStorage();
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['auth', 'cart']
+};
+
+const rootReducer = combineReducers({
+  auth: authReducer,
+  cart: cartReducer,
+  shop: shopReducer,
+  order: orderReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-  reducer: {
-    auth: authReducer,
-    cart: cartReducer,
-    shop: shopReducer,
-    order: orderReducer,
-  },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
-      serializableCheck: false,
+      serializableCheck: {
+        ignoredActions: [
+          'persist/PERSIST',
+          'persist/REHYDRATE',
+          'persist/PAUSE',
+          'persist/FLUSH',
+          'persist/PURGE',
+          'persist/REGISTER',
+        ],
+      },
     }),
 });
+
+export const persistor = persistStore(store);
 
 export default store;
